3f3e9774854ddb034b11e9a8003c0626423822f3
TASIN <116006207+Muhtadina@users.noreply.github.com>
2025-12-11 02:58:39 +0600

    supabase testing

---
+++ test.js
@@ -0,0 +1,10 @@
+// Only to text if the Supabase database works - 
+// involving .env with the API keys
+
+import supabase from "./src/config/SupabaseClient.js";
+
+const client = supabase.getClient();
+
+const { data, error } = await client.from("base_users").select("*");
+
+console.log(data, error);
